5ed9cca231a1470079497a64d223a3d4ce6f2643
Abir835 <abirhasan.raj.bd@gmail.com>
2024-09-12 11:25:02 +0600

    [Learning] --> Variable Initializer

---
+++ variable_initializer.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var x, y int = 1, 2
+
+func main() {
+	var python, java, c = true, false, "no!"
+	a := 10
+
+	var d bool
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	fmt.Println(a)
+	fmt.Println(python, java, c)
+	fmt.Println(x, y)
+}
