2aefa9ed87878d54966abe5b909e30b48e4cc6b2
abir835 <abirhasan.raj.bd@gmail.com>
2024-09-24 21:54:26 +0600

    channel practice

--- channel.go
+++ channel.go
@@ -1,22 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum // send sum to c
-}
+var wg sync.WaitGroup
 
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
 
-	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c // receive from c
+	channel := make(chan string, 3)
 
-	fmt.Println(x, y, x+y)
+	channel <- "hello world"
+	channel <- "hello world2"
+	channel <- "hello world3"
+
+	//close(channel)
+	//
+	//for elem := range channel {
+	//	fmt.Println(elem)
+	//}
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+	//go attack()
+	//time.Sleep(time.Second)
 }
+
+//func attack() {
+//	fmt.Println("attack")
+//}
